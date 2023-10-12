2039e51ebe1b28142fc8d885ab25db5c5b02807c
Axel Zwaans <axelzwaans@hotmail.com>
2023-10-12 10:30:41 +0100

    Finish question functionality

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,57 +7,13 @@ import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/home/HomeFilters";
 import QuestionCard from "@/components/cards/QuestionCard";
 import NoResult from "@/components/shared/NoResult";
+import { getQuestions } from "@/lib/actions/question.action";
 
-const questions = [
-  {
-    _id: "1",
-    title: "How to use React Query?",
-    tags: [
-      {
-        _id: "1",
-        name: "React",
-      },
-      {
-        _id: "2",
-        name: "React Query",
-      },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "john-doe.png",
-    },
-    upvotes: 1500000,
-    views: 500434,
-    answers: [],
-    createdAt: new Date(),
-  },
-  {
-    _id: "2",
-    title: "How to use React Query?",
-    tags: [
-      {
-        _id: "1",
-        name: "React",
-      },
-      {
-        _id: "2",
-        name: "React Query",
-      },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "john-doe.png",
-    },
-    upvotes: 10,
-    views: 20,
-    answers: [],
-    createdAt: new Date(),
-  },
-];
+export default async function Home() {
+  const result = await getQuestions({});
 
-export default function Home() {
+  console.log(result.questions);
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -86,8 +42,8 @@ export default function Home() {
       </div>
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
