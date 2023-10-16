0ab12f39b338ab6a3da9f53bf9afefdf830eddeb
Axel Zwaans <axelzwaans@hotmail.com>
2023-10-16 11:45:20 +0100

    Copy and paste example code for webhook.ts

--- app/api/webhook/webhook.ts
+++ app/api/webhook/webhook.ts
@@ -1,54 +1,71 @@
-import type { IncomingHttpHeaders } from "http";
-import type { NextApiRequest, NextApiResponse } from "next";
-import type { WebhookRequiredHeaders } from "svix";
-import type { WebhookEvent } from "@clerk/nextjs/server";
+/* eslint-disable camelcase */
 import { Webhook } from "svix";
-import { CreateUser, deleteUser } from "@/lib/actions/user.action";
-import { UpdateUser } from "@/lib/actions/user.action";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
-type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
-  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
-};
+export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
-const webhookSecret = process.env.NEXT_CLERK_WEBHOOK_SECRET;
-
-export default async function handler(
-  req: NextApiRequestWithSvixRequiredHeaders,
-  res: NextApiResponse
-) {
-  const payload = JSON.stringify(req.body);
-  const headers = req.headers;
-  // Create a new Webhook instance with your webhook secret
-  let wh: Webhook;
-  if (webhookSecret) {
-    wh = new Webhook(webhookSecret);
-  } else {
-    return res.status(400).json({});
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
   }
+
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  // Create a new SVIX instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
+
+  // Verify the payload with the headers
   try {
-    // Verify the webhook payload and headers
-    evt = wh.verify(payload, headers) as WebhookEvent;
-  } catch (_) {
-    // If the verification fails, return a 400 error
-    return res.status(400).json({});
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
+
   const eventType = evt.type;
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    // Create user in database
-    const mongoUser = await CreateUser({
+    // Create a new user in your database
+    const mongoUser = await createUser({
       clerkId: id,
+      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+      username: username!,
       email: email_addresses[0].email_address,
       picture: image_url,
-      username: username!,
-      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
     });
+
     return NextResponse.json({ message: "OK", user: mongoUser });
   }
 
@@ -56,17 +73,18 @@ export default async function handler(
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    // Create user in database
-    const mongoUser = await UpdateUser({
+    // Create a new user in your database
+    const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
+        name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+        username: username!,
         email: email_addresses[0].email_address,
         picture: image_url,
-        username: username!,
-        name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
       },
       path: `/profile/${id}`,
     });
+
     return NextResponse.json({ message: "OK", user: mongoUser });
   }
 
@@ -80,5 +98,5 @@ export default async function handler(
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  return res.status(200).json({});
+  return NextResponse.json({ message: "OK" });
 }
