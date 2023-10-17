1ac445075627161a7cbc03111207fd4d87718e29
Axel Zwaans <axelzwaans@hotmail.com>
2023-10-17 14:31:27 +0100

    Copy and paste route and user.action files

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -67,11 +67,16 @@ export async function deleteUser(params: DeleteUserParams) {
       throw new Error("User not found");
     }
 
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // Delete user from database
+    // and questions, answers, comments, etc.
 
+    // get user question ids
+    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
+
+    // delete user questions
     await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc.
 
     const deletedUser = await User.findByIdAndDelete(user._id);
 
