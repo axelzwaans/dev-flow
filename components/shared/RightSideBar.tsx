18b47fe372c3f96459e89fb75cc7e9564dd56268
Axel Zwaans <axelzwaans@hotmail.com>
2023-11-01 13:07:24 +0000

    Complete popular tags

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -2,39 +2,21 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-const hotQuestions = [
-  { id: "1", title: "How do I use express as a custom server in NextJS?" },
-  {
-    id: "2",
-    title:
-      "Would it be appropriate to point out an error in another paper during a referee report?",
-  },
-  { id: "3", title: "How can an airconditioning machine exist?" },
-  { id: "4", title: "Interrogated every time crossing UK Border as citizen" },
-  {
-    id: "5",
-    title: "What is an example of 3 numbers that do not make up a vector?",
-  },
-];
+const RightSideBar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
-const popularTags = [
-  { _id: "1", name: "javascript", totalQuestions: 5 },
-  { _id: "2", name: "react", totalQuestions: 7 },
-  { _id: "3", name: "next", totalQuestions: 5 },
-  { _id: "4", name: "vue", totalQuestions: 4 },
-  { _id: "5", name: "redux", totalQuestions: 10 },
-];
-
-const RightSideBar = () => {
   return (
     <section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[350px] custom-scrollbar">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {hotQuestions.map((question) => (
+          {hotQuestions?.map((question) => (
             <Link
-              href={`/questions/${question.id}`}
+              href={`/question/${question.id}`}
               key={question.id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
@@ -60,7 +42,7 @@ const RightSideBar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
